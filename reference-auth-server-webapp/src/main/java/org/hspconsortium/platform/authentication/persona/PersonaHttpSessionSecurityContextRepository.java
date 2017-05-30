a35a9d1f3e1b1f0c6150cfe5e3829f8e8d6c9868
Travis <travis@Travis-MacBook-Pro.local>
2017-05-29 23:47:48 -0600

    Fixed NPE when no cookies

--- reference-auth-server-webapp/src/main/java/org/hspconsortium/platform/authentication/persona/PersonaHttpSessionSecurityContextRepository.java
+++ reference-auth-server-webapp/src/main/java/org/hspconsortium/platform/authentication/persona/PersonaHttpSessionSecurityContextRepository.java
@@ -42,9 +42,11 @@ public class PersonaHttpSessionSecurityContextRepository extends HttpSessionSecu
 
         String personaJwtString = null;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(personaCookieName)) {
-                personaJwtString = cookie.getValue();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(personaCookieName)) {
+                    personaJwtString = cookie.getValue();
+                }
             }
         }
 
